35467423897bf218d04815de942f56e4908a38d6
L. Stuckstette <lstuckstette@gmail.com>
2018-06-26 15:58:10 +0200

    added adc/mcp3008 support; removed callbacks & ported to async

--- src/index.js
+++ src/index.js
@@ -1,26 +1,75 @@
 let Cylon = require("cylon");
 let dht22 = require("node-dht-sensor");
+let mcp3008 = require("./mcp3008");
+let adc = new mcp3008();
+let tempChannel = 1;
+let lightChannel = 2;
 
 console.log("Started:");
 
 Cylon.robot({
     connections: {
-        raspi: { adaptor: 'raspi' }
+        raspi: {adaptor: 'raspi'}
     },
 
     devices: {
-        led: { driver: 'led', pin: 11 }
+        led: {driver: 'led', pin: 11}
     },
 
-    work: function(my) {
-        every((2).second(), my.led.toggle);
+    work: function (my) {
+        every((1).second(), my.led.toggle);
+        //every((2).second(), readDHT22);
+        every((1).second(), readRawTemperature);
+        every((1).second(), readRawBrightness);
 
-        dht22.read(22, 27, function(err, temperature, humidity) { // 22= DHT22, 27 = GPIO27 !!
-            if (!err) {
-                console.log('temp: ' + temperature.toFixed(1) + 'C, ' +
-                    'humidity: ' + humidity.toFixed(1) + '%'
-                );
-            }
-        });
     }
 }).start();
+
+async function readRawTemperature() {
+    let value = await(adc.read(tempChannel));
+    console.log("raw temp: " + value);
+    return value;
+}
+
+async function readRawBrightness() {
+    let value = await(adc.read(lightChannel));
+    console.log("raw brightness: " + value);
+    return value;
+}
+
+async function readDHT22Temperature() { // let derp = await readDHT22Temperature();
+    return new Promise((resolve, reject) => {
+        dht22.read(22, 27, function (err, temperature, humidity) { // 22= DHT22, 27 = GPIO27 !!
+            if (!err) {
+                //  console.log('temp: ' + temperature.toFixed(1) + 'C, ' +
+                //      'humidity: ' + humidity.toFixed(1) + '%');
+                resolve(temperature.toFixed(1));
+            } else {
+                console.log("Error reading DHT22! not connected? " + err);
+                reject(err);
+
+            }
+        });
+    });
+}
+
+async function readDHT22Humidity() { // let derp = await readDHT22Humidity();
+    return new Promise((resolve, reject) => {
+        dht22.read(22, 27, function (err, temperature, humidity) { // 22= DHT22, 27 = GPIO27 !!
+            if (!err) {
+                //  console.log('temp: ' + temperature.toFixed(1) + 'C, ' +
+                //      'humidity: ' + humidity.toFixed(1) + '%');
+                resolve(humidity.toFixed(1));
+            } else {
+                console.log("Error reading DHT22! not connected? " + err);
+                reject(err);
+
+            }
+        });
+    });
+}
+
+function readDHT22() {
+
+
+}
---
+++ src/mcp3008.js
@@ -0,0 +1,50 @@
+/*
+SOURCE: https://github.com/fiskeben/mcp3008.js
+modified by lstuckstette 2018
+ */
+let SPI = require('pi-spi');
+
+let channels = [],
+    device = '/dev/spidev0.0',
+    spi;
+
+function isLegalChannel(channelNumber) {
+    if (typeof channelNumber !== 'number' || channelNumber < 0 || channelNumber > 7) {
+        throw new Error("Channel must be a number from 0 to 7");
+    }
+}
+
+function read(channel) {
+    if (spi === undefined)
+        return;
+    isLegalChannel(channel);
+
+    let txBuf = new Buffer(3);
+    txBuf.writeUInt8(1, 0);
+    txBuf.writeUInt8((8 + channel) << 4, 1);
+    txBuf.writeUInt8(0, 2);
+
+    return new Promise((resolve, reject) => {
+        spi.transfer(txBuf, function (err, buffer) {
+            if (err) reject(err);
+
+            let value = ((buffer[1] & 3) << 8) + buffer[2];
+            resolve(value);
+        });
+    });
+}
+
+
+var Mcp3008 = function (dev) {
+    device = dev || device;
+    spi = SPI.initialize(device);
+
+    this.read = read;
+
+
+    this.close = function () {
+        spi.close();
+    }
+};
+
+module.exports = Mcp3008;
